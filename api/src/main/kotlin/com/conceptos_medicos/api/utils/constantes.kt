f0ef5cf46755b93d385e8c77ce875c49bacdf80e
camilo hernandez <juaca2009@gmail.com>
2020-10-27 21:36:11 -0500

    endpoints conceptos funcionando

--- api/src/main/kotlin/com/conceptos_medicos/api/utils/constantes.kt
+++ api/src/main/kotlin/com/conceptos_medicos/api/utils/constantes.kt
@@ -7,5 +7,7 @@ class constantes {
         private const val url_base = url_api_base + url_tipo
         //endpoints para vocavularios
         const val url_base_vocabularios = "$url_base/vocabularios" 
+        //endpoints para conceptos
+        const val url_base_conceptos = "$url_base/conceptos"
     }
 }
---
+++ api/src/main/kotlin/com/conceptos_medicos/api/web/conceptoRestController.kt
@@ -0,0 +1,70 @@
+package com.conceptos_medicos.api.web
+
+import com.conceptos_medicos.api.business.iconcepto
+import com.conceptos_medicos.api.utils.constantes
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import com.conceptos_medicos.api.exception.businessException
+import com.conceptos_medicos.api.model.concepto
+import com.conceptos_medicos.api.exception.notFoundException
+import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping(constantes.url_base_conceptos)
+class conceptoController {
+    @Autowired
+    val conceptoBusiness: iconcepto? = null
+
+    @GetMapping("")
+    fun obtener_concepto(): ResponseEntity<List<concepto>>{
+        return try {
+            ResponseEntity(conceptoBusiness!!.obtener_concepto(), HttpStatus.OK)
+        }
+        catch(e: businessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @GetMapping("/{id}")
+    fun cargar_vocabulario(@PathVariable("id") idconc:Long): ResponseEntity<Any>{
+        return try {
+            ResponseEntity(conceptoBusiness!!.cargar_concepto(idconc), HttpStatus.OK)
+        }
+        catch(e: businessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+        catch(e: notFoundException){
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+    @PostMapping("")
+    fun guardar_concepto(@RequestBody concep: concepto): ResponseEntity<Any>{
+        return try {
+            conceptoBusiness!!.guardar_concepto(concep)
+            val header_respuesta = HttpHeaders()
+            header_respuesta.set("location", constantes.url_base_conceptos + "/" + concep.idCODE)
+            ResponseEntity(header_respuesta, HttpStatus.CREATED)
+        }
+        catch(e: businessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    fun eliminar_concepto(@PathVariable("id") idconc: Long): ResponseEntity<Any>{
+        return try {
+            conceptoBusiness!!.eliminar_concepto(idconc)
+            ResponseEntity(HttpStatus.OK)
+        }
+        catch(e: businessException) {
+            ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+        catch(e: notFoundException){
+            ResponseEntity(HttpStatus.NOT_FOUND)
+        }
+    }
+
+}
